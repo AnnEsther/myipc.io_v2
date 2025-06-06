334f75440a54777b4fcacda690eede4c65581ae3
AnnE <annjoymathew@gmail.com>
2025-06-06 13:06:13 -0700

    removing db creation in start.sh

--- start.sh
+++ start.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 
 function prd() { 
-    set -e
-    trap "echo 'Stopping Postgres...'; su - postgres -c 'pg_ctl -D /tmp/pgdata stop'" EXIT
-
-    echo "Starting PostgreSQL..."
-    service postgresql start
-
-    if [ ! -d /tmp/pgdata ]; then
-        echo "Initializing DB..."
-        su - postgres -c "initdb -D /tmp/pgdata"
-    fi
-
-    echo "Starting Postgres..."
-    su - postgres -c "pg_ctl -D /tmp/pgdata -l /tmp/logfile start"
-
-    sleep 3
-
-    echo "=> Running IPC Install"
-    node ipc-install || echo "IPC install failed"
-
-    echo "=> Starting Express App"
-    node index || echo "App failed to start"
+    echo "=> Starting Node App"
+    node index
  }
 
 function debug() { sleep 999999999 ; }
